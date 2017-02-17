6acd690c5e329172a9b64afbccc9591a9e7fe404
Daniel Sousa <sousa.dfs@gmail.com>
2017-02-17 21:01:30 -0200

    Primeiro Commit

---
+++ app/src/main/java/com/example/a1514290074/teste/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1514290074.teste;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
