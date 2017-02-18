a560c18e925d6d6582c3ba675aa255b8a46070d8
Daniel Sousa <sousa.dfs@gmail.com>
2017-02-18 03:48:16 -0200

    Modifica Tela de Login
    Adiciona icone
    Muda cor
    Adciona novos EditText (TextInputLayout)

--- app/src/main/java/com/example/a1514290074/teste/CadastroActivity.java
+++ app/src/main/java/com/example/a1514290074/teste/CadastroActivity.java
@@ -3,11 +3,11 @@ package com.example.a1514290074.teste;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class CadastroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cadastro);
     }
 }
